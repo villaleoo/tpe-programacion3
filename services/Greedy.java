48924f696fbdecc92cba2cc66ff9395d635003b2
leopoldo <villaleoo@hotmail.com>
2024-06-02 01:55:58 -0300

    mejorando sintaxis y agregado trycatch para lista de tareas que exceda las restricciones

--- services/Greedy.java
+++ services/Greedy.java
@@ -4,7 +4,6 @@ import tpe.structures.Processor;
 import tpe.structures.Task;
 import tpe.utils.CSVReader;
 
-import java.lang.reflect.Array;
 import java.util.*;
 
 //para resolver el problema por greedy se elaboraron los siguientes pasos:
@@ -13,13 +12,13 @@ import java.util.*;
 //3) se creo la funcion getAssigment que seria la funcion de estructura general/clasica de greedy:
     //se tiene un conjunto de solucion inicialmente vacio (solo con los procesadores cargados, sin tareas asignadas)
     //se itera la cola de tareas (candidatos) hasta llegar a estar vacia. En esta iteracion se busca el procesador que minimiza el tiempo de ejecucion total para asignarle la primer tarea de la cola
-    //la cola de tareas va disminuyendo a medida que el algoritmo va encontrando procesadores adecuados para esa tarea (funcion betterProcc)
+    //la cola de tareas va disminuyendo a medida que el algoritmo va encontrando procesadores adecuados para esa tarea (funcion getBetterProccesor)
 
-//4)la funcion betterProcc recibe una tarea (la primera de la cola) de getAssigment e itera los procesadores buscando los adecuados para asignarle esa tarea (funcion greedyIsFact).
-    //para determinar si un procesador es adecuado para una tarea, se creo la funcion greedyIsFact ---> de acuerdo a una tarea y un procesador, determina si
+//4)la funcion getBetterProccesor recibe una tarea (la primera de la cola) de getAssigment e itera los procesadores buscando los adecuados para asignarle esa tarea (funcion isFactible).
+    //para determinar si un procesador es adecuado para una tarea, se creo la funcion isFactible ---> de acuerdo a una tarea y un procesador, determina si
     //el procesador cumple con los requisitos para asignarle esa tarea.
-    //betterProcc a medida que va encontrando procesadores aptos, los va agregando a una lista de procesadores posibles
-    //cuando termina de obtener la lista de los procesadores posibles, se los envia a la funcion getProcMinTimeEjec para obtener, entre los posibles procesadores,
+    //getBetterProccesor a medida que va encontrando procesadores aptos, los va agregando a una lista de procesadores posibles
+    //cuando termina de obtener la lista de los procesadores posibles, se los envia a la funcion getProccShortExectTime para obtener, entre los posibles procesadores,
     //el procesador que no tiene ninguna tarea asignada (seria el mejor para minimizar tiempos) ó el que menor tiempo de ejecucion tiene al momento de asignar la tarea.
 
 //5) una vez que se determina el mejor procesador para la primer tarea de la cola siguiendo los pasos del punto (4), getAssigment consigue el mejor procesador para
@@ -27,17 +26,17 @@ import java.util.*;
 
 //SE LLEGA A LA SOLUCION UNA VEZ QUE LA COLA DE TAREAS ESTA VACIA. SE ITERA CADA TAREA, BUSCANDO EL PROCESADOR ADECUADO QUE MINIMIZA EL TIEMPO DE EJECUCION
 //getAssigment -> itera cada tarea para asignarle el procesador que minimiza el tiempo de ejecucion total.
-//betterProcc -> itera cada procesador para encontrar el conjunto de procesadores que se les puede asignar la tarea.
-//greedyIsFact -> la utiliza betterProcc para determinar cual procesador es apto para la tarea y cual no es apto.
-//getProcMinTimeEjec -> la utiliza betterProcc para determinar el procesador que minimiza el tiempo de ejecucion total. Trabaja con procesadores que son adecuados para 1 tarea
+//getBetterProccesor -> itera cada procesador para encontrar el conjunto de procesadores que se les puede asignar la tarea.
+//isFactible -> la utiliza getBetterProccesor para determinar cual procesador es apto para la tarea y cual no es apto.
+//getProccShortExectTime -> la utiliza getBetterProccesor para determinar el procesador que minimiza el tiempo de ejecucion total. Trabaja con procesadores que son adecuados para 1 tarea
 
 //EL TIEMPO DE EJECUCION DE UN PROCESADOR ES LA SUMATORIA DEL TIEMPO DE EJECUCION DE LAS TAREAS QUE TIENE ASIGNADO.
 
 public class Greedy {
-    ArrayList<Processor> proccList;
-    Queue<Task> taskQueue;
-    HashMap<String, ArrayList<Task>> solution;
-    int qTask;
+    private ArrayList<Processor> proccList;
+    private Queue<Task> taskQueue;
+    private HashMap<String, ArrayList<Task>> solution;
+    private int qTask;
 
     public Greedy(String pathProcessor, String pathTasks){
         CSVReader reader= new CSVReader();
@@ -47,13 +46,13 @@ public class Greedy {
 
         this.proccList=new ArrayList<>(reader.getProcessors());
         this.solution=new HashMap<>();
-        this.orderList(tasksList);
+        this.sortList(tasksList);
         this.taskQueue= new LinkedList<>(tasksList);
         this.qTask=this.taskQueue.size();
 
     }
 
-    private void orderList (ArrayList<Task> tasks){
+    private void sortList (ArrayList<Task> tasks){
         //ORDENA LA LISTA DE TAREAS POR TIEMPO DE EJECUCION Y NIVEL DE PRIORIDAD -> ante iguales tiempo de ejecucion, prioritarias primero.
         //se ordeno las que mas tiempo de ejecucion conllevan primero para minimizar el tiempo de ejecucion final.
         Comparator<Task> compPriorityAndEjecTime = new Comparator<Task>() {
@@ -74,9 +73,17 @@ public class Greedy {
     //funcion que imprime en pantalla la solucion del servicio
     public void printAssigment(Float x){
 
-        this.getAssigment(x);
+        try{
+            this.getAssigment(x);
 
-        ArrayList<String> dataProccBigTime= this.getDataProccBigTime();
+        }catch(Exception e){
+            System.out.println("Hay tareas que con estas restricciones no pueden ser asignadas. Quedaron "+this.taskQueue.size()+" en cola.");
+            System.out.println("Solucion maxima a la que se llego:\n");
+        }
+
+
+        ArrayList<String> dataProccBigTime= this.getDataProccBigExecTime();
+
         System.out.println("\n\tEl procesador con mayor tiempo de ejecucion es: id_procesador = "+dataProccBigTime.getFirst()+";");
         System.out.println("\tTIEMPO TOTAL DE EJECUCION : "+dataProccBigTime.getLast()+";");
         System.out.println("\tTOTAL DE TAREAS (candidatos) ASIGNADAS A PROCESADORES: "+this.qTask+";");
@@ -92,12 +99,35 @@ public class Greedy {
             this.solution.put(p.getIdProc(),new ArrayList<>());
         }
 
-       while(!this.taskQueue.isEmpty()){
-           Processor p = this.betterProcc(this.taskQueue.peek(),x);
 
-           this.solution.get(p.getIdProc()).add(this.taskQueue.poll());
+        while(!this.taskQueue.isEmpty()){
+            Processor p = this.getBetterProccesor(this.taskQueue.peek(),x);
 
-       }
+            this.solution.get(p.getIdProc()).add(this.taskQueue.poll());
+
+        }
+
+
+       //si la capacidad de los no refrigerados esta a tope && (todos los refrigerados tienen 2 criticas && la que viene es critica) || la que viene es critica y quedan procesadores sin 2 criticas
+
+    }
+
+    //Esta funcion primero busca los procesadores que son candidatos a poder asignarle las tareas y los agrega a la lista de posibles
+    //Cuando obtiene la lista de los que son adecuados para la tarea, se la pasa a otra funcion que busca y
+    //obtiene el procesador con el tiempo minimo de ejecucion al momento, con el fin de achicar el tiempo maximo de ejecucion
+    private Processor getBetterProccesor(Task t, Float X) {
+        ArrayList<Processor> possibles=new ArrayList<>();
+        Processor pMin;
+
+        for(Processor p: this.proccList){
+            if(this.isFactible(p,t,X)){
+                possibles.add(p);
+            }
+        }
+
+        pMin= this.getProccShortExectTime(possibles);
+
+        return pMin;
 
     }
 
@@ -109,28 +139,49 @@ public class Greedy {
         //si el procesador ES refrigerado, se observa si la tarea es critica
             //si la tarea es critica, retorno true/false si el procesador tiene menos de 2 tareas criticas
             //-->si la tarea no es critica, el procesador refrigerado se considera adecuado para la tarea.
-    private boolean greedyIsFact(Processor p, Task t,Float x){
-        float totalTimeEjectP= this.totalTimeEject(this.solution.get(p.getIdProc()));
+    private boolean isFactible(Processor p, Task t,Float x){
+        float getTimeExectTaskListP= this.getTimeExectTaskList(this.solution.get(p.getIdProc()));
 
+        int MAX_CRITIC_TASKS = 2;
         if(!p.isCooled()){
-            if(totalTimeEjectP <= x){
-                if(totalTimeEjectP + t.getTiempo_ejecucion() > x){
+            if(getTimeExectTaskListP <= x){
+                if(getTimeExectTaskListP + t.getTiempo_ejecucion() > x){
                     return false;
                 }else{
                     if((t.isEsCritica())){
-                        return this.getQuantityCriticTaskInProcc(p) < 2;
+                        return this.getQuantityCriticTaskInProcc(p) < MAX_CRITIC_TASKS;
                     }
                     return true;
                 }
             }
         }else{
             if(t.isEsCritica()){
-                return this.getQuantityCriticTaskInProcc(p) < 2;
+                return this.getQuantityCriticTaskInProcc(p) < MAX_CRITIC_TASKS;
             }
             return true;
         }
 
         return false;
+    }
+    
+    //entre los procesadores que cumplen los requisitos para asignarle la tarea,
+    //retorna el procesador que se encuentra vacio Ó el que tiene menor tiempo de ejecucion al momento
+    private Processor getProccShortExectTime(ArrayList<Processor> proccs){
+        float accTimeEjec=this.getTimeExectTaskList(this.solution.get(proccs.getFirst().getIdProc()));
+        Processor pMin=proccs.getFirst();
+
+        for(Processor p: proccs){
+            if(this.solution.get(p.getIdProc()).isEmpty()){
+                return p;
+            }else{
+                if(this.getTimeExectTaskList(this.solution.get(p.getIdProc())) < accTimeEjec){
+                    pMin=p;
+                }
+            }
+
+        }
+        
+        return pMin;
     }
 
     //retorna la cantidad de tareas criticas que tiene asignado el procesador
@@ -143,57 +194,17 @@ public class Greedy {
         }
         return q;
     }
-
-    //Esta funcion primero busca los procesadores que son candidatos a poder asignarle las tareas y los agrega a la lista de posibles
-    //Cuando obtiene la lista de los que son adecuados para la tarea, se la pasa a otra funcion que busca y
-    //obtiene el procesador con el tiempo minimo de ejecucion al momento, con el fin de achicar el tiempo maximo de ejecucion
-    private Processor betterProcc(Task t, Float X) {
-        ArrayList<Processor> possibles=new ArrayList<>();
-        Processor pMin;
-
-        for(Processor p: this.proccList){
-            if(this.greedyIsFact(p,t,X)){
-                possibles.add(p);
-            }
-        }
-
-        pMin= this.getProcMinTimeEjec(possibles);
-
-        return pMin;
-
-    }
-
-    //entre los procesadores que cumplen los requisitos para asignarle la tarea,
-    //retorna el procesador que se encuentra vacio Ó el que tiene menor tiempo de ejecucion al momento
-    private Processor getProcMinTimeEjec(ArrayList<Processor> proccs){
-        float accTimeEjec=this.totalTimeEject(this.solution.get(proccs.getFirst().getIdProc()));
-        Processor pMin=proccs.getFirst();
-
-        for(Processor p: proccs){
-            if(this.solution.get(p.getIdProc()).isEmpty()){
-                return p;
-            }else{
-                if(this.totalTimeEject(this.solution.get(p.getIdProc())) < accTimeEjec){
-                    pMin=p;
-                }
-            }
-
-        }
-
-        return pMin;
-    }
-
+    
     //recibe una lista de tareas y retorna la sumatoria del tiempo de ejecucion
-    private float totalTimeEject (ArrayList<Task> tasksAssigs){
+    private float getTimeExectTaskList (ArrayList<Task> tasksAssigs){
         float acc=0;
         for(Task t : tasksAssigs){
             acc= acc+ t.getTiempo_ejecucion();
         }
         return acc;
     }
-
-
-    private ArrayList<String> getDataProccBigTime (){
+    
+    private ArrayList<String> getDataProccBigExecTime (){
         ArrayList<String> dataProcc = new ArrayList<>();
         float total=0;
         float tmp;
@@ -213,9 +224,7 @@ public class Greedy {
 
         return dataProcc;
     }
-
-
-
+    
 }
 
 
