520871ad17095e049a720e2a654d16ed97a9bf1f
gabriela christensen <gchristensen@alumnos.exa.unicen.edu.ar>
2024-06-03 18:31:29 -0300

    Actualización main

--- Main.java
+++ Main.java
@@ -15,43 +15,43 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Por favor, ingrese el valor X para minimizar el tiempo de ejecución: ");
         float x = scanner.nextFloat();
-        Greedy g = new Greedy("datasets/Procesadores.csv", "datasets/Tareas.csv");
+        Greedy g = new Greedy("./src/tpe/datasets/Procesadores.csv", "./src/tpe/datasets/Tareas.csv");
         g.printAssigment(x);
 
 
-//        Services services = new Services("datasets/Procesadores.csv", "datasets/Tareas.csv");
-        Backtracking backtracking = new Backtracking("datasets/Procesadores.csv", "datasets/Tareas.csv");
+        Services services = new Services("./src/tpe/datasets/Procesadores.csv", "./src/tpe/datasets/Tareas.csv");
+        Backtracking backtracking = new Backtracking("./src/tpe/datasets/Procesadores.csv", "./src/tpe/datasets/Tareas.csv");
 
-//
-//        System.out.println("---------- SERVICIO 1 ----------");
-//        System.out.print("Por favor, ingrese el valor de la tarea a buscar: ");
-//        String valor = scanner.nextLine();
-//        System.out.println("RESULTADO SERVICIO 1: ");
-//        Task getTaskById = services.service1(valor);
-//        if (getTaskById != null)
-//            System.out.println(getTaskById);
-//
-//        System.out.println("--------- SERVICIO 2 -----------");
-//
-//        System.out.print("Ingrese 1 para obtener las tareas " +
-//                "criticas o 0 para obtener las tareas no criticas: ");
-//
-//        int critic = scanner.nextInt();
-//        List<Task> taskByCritic = services.servicio2(critic == 1);
-//
-//        System.out.println("RESULTADO SERVICIO 2:\n");
-//        System.out.println(taskByCritic);
-//
-//
-//        System.out.println("--------- SERVICIO 3 -----------");
-//        System.out.print("Ingrese prioridad inferior: ");
-//        int priorityInf = scanner.nextInt();
-//        System.out.print("Ingrese prioridad superior: ");
-//        int prioritySup = scanner.nextInt();
-//        System.out.println("RESULTADO SERVICIO 3:\n");
-//        List<Task> getTasksByPriorities = services.servicio3(priorityInf, prioritySup);
-//        if (getTasksByPriorities != null && !getTasksByPriorities.isEmpty())
-//            System.out.println(getTasksByPriorities);
+
+        System.out.println("---------- SERVICIO 1 ----------");
+        System.out.print("Por favor, ingrese el valor de la tarea a buscar: ");
+        String valor = scanner.nextLine();
+        System.out.println("RESULTADO SERVICIO 1: ");
+        Task getTaskById = services.service1(valor);
+        if (getTaskById != null)
+            System.out.println(getTaskById);
+
+        System.out.println("--------- SERVICIO 2 -----------");
+
+        System.out.print("Ingrese 1 para obtener las tareas " +
+                "criticas o 0 para obtener las tareas no criticas: ");
+
+        int critic = scanner.nextInt();
+        List<Task> taskByCritic = services.servicio2(critic == 1);
+
+        System.out.println("RESULTADO SERVICIO 2:\n");
+        System.out.println(taskByCritic);
+
+
+        System.out.println("--------- SERVICIO 3 -----------");
+        System.out.print("Ingrese prioridad inferior: ");
+        int priorityInf = scanner.nextInt();
+        System.out.print("Ingrese prioridad superior: ");
+        int prioritySup = scanner.nextInt();
+        System.out.println("RESULTADO SERVICIO 3:\n");
+        List<Task> getTasksByPriorities = services.servicio3(priorityInf, prioritySup);
+        if (getTasksByPriorities != null && !getTasksByPriorities.isEmpty())
+            System.out.println(getTasksByPriorities);
 
         System.out.println("\n ----------- BACKTRACKING ----------");
 
